f22e5d8771e20b0d751196cca4b2dc6b16875f54
Jack Sibley <jumbo20007@gmail.com>
2022-11-10 20:53:55 +0000

    ok go back to how it was before

--- app/run.sh
+++ app/run.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-echo "Starting the application..."
-echo "$PWD"
 set -eo pipefail
 
 # ensure app\resources\generated and app\static\raw_docs\ are present
-mkdir -p resources/generated
-mkdir -p static/raw_docs/
+mkdir -p app/resources/generated/
+mkdir -p app/static/raw_docs/
 
 # mount GCS buckets
-gcsfuse --debug_gcs --debug_fuse --only-dir generated/ ancestral-vision-storage resources/generated/
-gcsfuse --debug_gcs --debug_fuse --only-dir raw_docs/ ancestral-vision-storage static/raw_docs/
+gcsfuse --debug_gcs --debug_fuse --only-dir generated/ ancestral-vision-storage app/resources/generated/
+gcsfuse --debug_gcs --debug_fuse --only-dir raw_docs/ ancestral-vision-storage app/static/raw_docs/
 
 # run the app
-poetry run uvicorn main:app --proxy-headers --host 0.0.0.0 --port 80
+poetry run uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 80
 
 wait -n
