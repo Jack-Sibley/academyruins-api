eab96e92363e3fcebbce2536840889149d3eaf8c
Jack Sibley <jumbo20007@gmail.com>
2022-11-10 20:34:05 +0000

    run file

--- app/run.sh
+++ app/run.sh
@@ -2,14 +2,14 @@
 set -eo pipefail
 
 # ensure app\resources\generated and app\static\raw_docs\ are present
-mkdir -p app/resources/generated
-mkdir -p app/static/raw_docs/
+mkdir -p resources/generated
+mkdir -p static/raw_docs/
 
 # mount GCS buckets
-gcsfuse --debug_gcs --debug_fuse --only-dir generated/ ancestral-vision-storage app/resources/generated
-gcsfuse --debug_gcs --debug_fuse --only-dir raw_docs/ ancestral-vision-storage app/static/raw_docs/
+gcsfuse --debug_gcs --debug_fuse --only-dir generated/ ancestral-vision-storage resources/generated/
+gcsfuse --debug_gcs --debug_fuse --only-dir raw_docs/ ancestral-vision-storage static/raw_docs/
 
 # run the app
-poetry run uvicorn app.main:app --proxy-headers --host 0.0.0.0 --port 80
+poetry run uvicorn main:app --proxy-headers --host 0.0.0.0 --port 80
 
 wait -n
